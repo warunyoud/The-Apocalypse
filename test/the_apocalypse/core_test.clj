27f77e69a9555c6c099b6408cc7af4b9563490ea
warunyoud <warunyou_d@yahoo.com>
2016-11-13 13:56:00 -0600

    project created

---
+++ test/the_apocalypse/core_test.clj
@@ -0,0 +1,7 @@
+(ns the-apocalypse.core-test
+  (:require [clojure.test :refer :all]
+            [the-apocalypse.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
