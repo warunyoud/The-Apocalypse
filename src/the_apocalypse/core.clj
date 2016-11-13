71862817c5c9beb9b912bfd385c1db711ee8f797
warunyoud <warunyou_d@yahoo.com>
2016-11-13 15:21:14 -0600

    printTyping

--- src/the_apocalypse/core.clj
+++ src/the_apocalypse/core.clj
@@ -1,7 +1,32 @@
 (ns the-apocalypse.core
   (:gen-class))
 
+(defn printTyping [text]
+	(doseq [i (range (count text))]
+		(print (subs text i (+ i 1)))
+	(flush)
+	(Thread/sleep 100))
+	(println "")
+	)
+	
+(defn print-message [pid pw] 
+(println "PID : " pid)
+ (println "PW : " pw))
+
+(defn inp[]
+(println "Enter your PID and password") 
+(let[pid (read-line) pw (read-line)] 
+(print-message pid pw) ))
+
+(defrecord Person [fname health])
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (printTyping "Enter your name")
+  )
+
+
+
+
+
