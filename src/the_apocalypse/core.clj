f193662e29038d1b6787511c53fd983ea7c7b792
warunyoud <warunyou_d@yahoo.com>
2016-11-28 18:14:49 -0600

    yes

--- src/the_apocalypse/core.clj
+++ src/the_apocalypse/core.clj
@@ -22,59 +22,71 @@
     (println)
     ))
 
-(defn print-maze [arr, width, height]
+(defn print-maze [player]
+  (let []
   (doseq [i (range 0 (* width height))]
     (if (= (mod i width) 0) (println) "no")
     (print (nth arr i)))
     (println)
+  ))
+
+(def the-maze
+  {:first_room {:desc "best building"
+              :rightWall [0,0,0,
+                          0,1,0,
+                          0,0,0]
+              :downWall [0,1,0,
+                         0,0,0
+                         0,0,0]
+              :width 3
+              :height 3
+          :connection [{:x1 0 :y1 0 :maze second_room :x2 1 :y2 2
+            },{:x1 1 :y 2 :maze third_room :x2 0 :y2 2
+            }
+          ]
+        }
+    }
+  {:second_room {:desc "best building"
+              :rightWall [0,0,0,
+                          0,1,0,
+                          0,0,0]
+              :downWall [0,1,0,
+                         0,0,0
+                         0,0,0]
+              :width 3
+              :height 3
+          :connection [{:x1 0 :y1 0 :maze second_room :x2 1 :y2 2
+            },{:x1 1 :y 2 :maze third_room :x2 0 :y2 2
+            }
+          ]
+        }
+    }
   )
 
-; (def maze-array
-;   {:grainger {:array (read-maze "grainger.txt")
-;           ; :entrance [{:x 5 :y 8 :map grainger-north-entrance
-;           ;   },{:x 10 :y 15 :map grainger-south-entrance
-;           ;   }
-;           ; ]
-;         }
-;   }
-;   )
-
-
-; (def the-map
-;   {:tomb-north-entrance {:desc "The walls are freshly painted but do not have any pictures.  You get the feeling it was just created
-; for a game or something."
-;             :title "in the foyer"
-;             :dir {:south tomb-north-entrance}
-;             :maze {:array tomb :x 5 :y 8}
-;             :contents #{:raw-egg}}
-;     :grue-pen {:desc "It is very dark.  You are about to be eaten by a grue."
-;               :title "in the grue pen"
-;               :dir {:north :foyer}
-;               :contents #{}}
-
-;    })
-
-; (def adventurer
-;   {:location :foyer
-;    :inventory #{}
-;    :tick 0
-;    :seen #{}
-;    :x 0
-;    :y 0})
+(def adventurer
+  {:location :grainger
+   :inventory #{}
+   :seen #{}
+   :x
+   :y})
 
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (def arr [0,1,0,0,0,0,0,0,0])
-  (read-maze "src/the_apocalypse/text.txt" [0,1,0,0,0,0,0,0,0])
+  (loop [local-maze the-maze
+         local-player adventurer]
+    (let [_ (print-maze local-player)
+          _ (println-typing "What do you want to do?" 50)
+          command (read-line)]
+      (recur local-maze local-player))))
+
   ;(print-maze arr 3 3)
   ; (print-many-lines 30)
   ; (println-typing "THE APOCALYPSE - LIFE AFTER CS 225" 50)
   ; (print-typing "Welcome to UIUC in the year of 2020" 50)
   ; (println-typing "..." 300)
   ; (println-typing "after the aftermath of CS 225" 100)
-  )
 
 
 
