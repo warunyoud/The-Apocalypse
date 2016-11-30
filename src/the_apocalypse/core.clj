8d025448e3ac63eda2da9020bf1a8ad59a860297
warunyoud <warunyou_d@yahoo.com>
2016-11-29 18:34:21 -0600

    new

--- src/the_apocalypse/core.clj
+++ src/the_apocalypse/core.clj
@@ -22,31 +22,26 @@
     (println)
     ))
 
-(defn print-maze [player]
-  (let []
-  (doseq [i (range 0 (* width height))]
-    (if (= (mod i width) 0) (println) "no")
-    (print (nth arr i)))
-    (println)
-  ))
+
 
 (def the-maze
   {:first_room {:desc "best building"
+              :title "first_room"
               :rightWall [0,0,0,
-                          0,1,0,
-                          0,0,0]
+                          1,1,0,
+                          0,1,0]
               :downWall [0,1,0,
                          0,0,0
                          0,0,0]
               :width 3
               :height 3
-          :connection [{:x1 2 :y1 3 :maze second_room :x2 1 :y2 2
-            },{:x1 1 :y 2 :maze third_room :x2 0 :y2 2
+          :connection [{:x1 2 :y1 3 :maze :second_room :x2 1 :y2 2
+            },{:x1 1 :y 2 :maze :third_room :x2 0 :y2 2
             }
           ]
         }
-    }
-  {:second_room {:desc "best building"
+  :second_room {:desc "best building"
+              :title "second_room"
               :rightWall [0,0,0,
                           0,1,0,
                           0,0,0]
@@ -55,13 +50,58 @@
                          0,0,0]
               :width 3
               :height 3
-          :connection [{:x1 1 :y1 2 :maze second_room :x2 0 :y2 0
-            },{:x1 1 :y 2 :maze third_room :x2 0 :y2 2
+          :connection [{:x1 1 :y1 2 :maze :first_room :x2 0 :y2 0
+            },{:x1 1 :y 2 :maze :third_room :x2 0 :y2 2
+            }
+          ]
+        }
+    :third_room {:desc "best building"
+              :title "third_room"
+              :rightWall [0,0,0,
+                          0,1,0,
+                          0,0,0]
+              :downWall [0,1,0,
+                         0,0,0
+                         0,0,0]
+              :width 3
+              :height 3
+          :connection [{:x1 1 :y1 2 :maze :first_room :x2 0 :y2 0
+            },{:x1 1 :y 2 :maze :second_room :x2 0 :y2 2
             }
           ]
         }
     }
   )
+
+(defn print-maze [player]
+  (let [loc (player :location)
+        rightWall (-> the-maze loc :rightWall)
+        downWall (-> the-maze loc :downWall)
+        width (-> the-maze loc :width)
+        height (-> the-maze loc :height)]
+
+  (doseq [i (range 0 width)] (print " ___"))
+
+  (doseq [i (range 0 (* width height))]
+    
+
+    ;top
+    (if (= (mod i width) 0) (do (if (= i 0) (println) (println "|")) (print "|")
+
+    (doseq [j (range 0 width)] (print "   ")
+    (if (= (nth rightWall (+ j i)) 1) (print "|") (if (not= j (- width 1)) (print " "))))
+
+    (println "|") (print "|")))
+
+    ;second
+    (if (or (= (nth downWall i) 1) (>= (+ i width) (* width height)))(if (and (= (nth rightWall i) 0) (not= (mod (+ i 1) width) 0))(print "___ ") (print "___")))
+    (if (= (nth rightWall i) 1) (if (and (= (nth downWall i) 0) (< (+ i width) (* width height)))  (print "   |") (print "|")))
+    (if (and (= (nth downWall i) 0) (= (nth rightWall i) 0) (< (+ i width) (* width height))) (if (not= (mod (+ i 1) width) 0) (print "    ") (print "   ")))
+  )
+  (println "|"))
+
+  (println))
+
 
 (defn can_travel [player]
   )
@@ -73,16 +113,25 @@
    :x
    :y})
 
+(defn status [player]
+  ; (let [location (player :location)]
+  ;   (print (str "You are " (-> the-map location :title) ". "))
+  ;   (when-not ((player :seen) location)
+  ;     (print (-> the-map location :desc)))
+  ;   (update-in player [:seen] #(conj % location)))
+  (player)
+)
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (loop [local-maze the-maze
          local-player adventurer]
-    (let [_ (print-maze local-player)
-          _ (println-typing "What do you want to do?" 50)
+        (let [_ (println-typing "What do you want to do?" 50) 
           command (read-line)]
+          (print-maze local-player)
       (recur local-maze local-player))))
+
 
   ;(print-maze arr 3 3)
   ; (print-many-lines 30)
