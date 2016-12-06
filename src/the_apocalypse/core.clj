52b8e87cc7ec9b6d49db3df5db1be95961b8864b
warunyoud <warunyou_d@yahoo.com>
2016-12-05 19:40:30 -0600

    Finished work

--- src/the_apocalypse/core.clj
+++ src/the_apocalypse/core.clj
@@ -38,6 +38,9 @@
 |                                      |
 | MAP USAGE                            |
 | To use the map - Enter use map       |
+|                                      |  
+| NOTHING                              |
+| To do nothing - Type nothing         |
 |______________________________________|")
   player)
 
@@ -71,8 +74,8 @@
 ;(println "Yo whatsup")
 ; definition of the 3 mazes
 (def the-maze
-  {:first_room {:desc "first room in an unknown building"
-              :title "first_room"
+  {:first_room {:desc "You are now in Room1 of an unknown building in an unknown world."
+              :title "Room1"
               :rightWall [0,0,1,0
                           1,1,0,0
                           0,1,0,0]
@@ -90,8 +93,8 @@
           :monsters []
         }
 
-  :second_room {:desc "second room in an unknown building. There is a monster here"
-              :title "second_room"
+  :second_room {:desc "You are now in Room2 of an unknown building in an unknown world. You can hear noises of an unknown creature in a distance."
+              :title "Room2"
               :rightWall [0,0,0,0,0
                           1,0,0,1,0
                           1,1,0,1,0
@@ -104,9 +107,9 @@
                          0,0,0,0,0]
               :width 5
               :height 5
-          :exit 1
-          :key 3
-          :map 5
+          :exit 12
+          :key 24
+          :map 20
           :nextmap :third_room
           :n 5 
           :monstn 1
@@ -115,8 +118,8 @@
                     :path [17,16,11,6,7,8,13,18]}]
         }
     
-    :third_room {:desc "Third room in an unknown building. There are 2 monsters in this maze"
-              :title "third_room"
+    :third_room {:desc "You are now in Room3 of an unknown building in an unknown world. You can hear noises of unknown number of creatures in a distance."
+              :title "Room3"
               :rightWall [0,0,0,0
                           0,0,1,0
                           0,1,0,0
@@ -129,15 +132,15 @@
                          0,0,0,0]
               :width 4
               :height 5
-          :exit 16
+          :exit 0
           :key 6
           :nextmap :exit
           :map 10
-          :n 0
+          :n 2
           :monstn 2
           :monsters [{:index 0
-                    :size 8
-                    :path [19, 15, 11, 7, 3, 7, 11, 15]}
+                    :size 14
+                    :path [16, 17, 18, 19, 15, 11, 7, 3, 7, 11, 15, 19, 18, 17]}
                     {:index 0
                     :size 10
                     :path [14, 13, 12, 8, 4, 5, 4, 8, 12, 13]}]
@@ -162,6 +165,19 @@
     (= dir 3) (not (or (< curr_loc width) (= (nth downWall (- curr_loc width)) 1)))
 
     (= dir 1) (not (or (>= (+ curr_loc width) (* width height)) (= (nth downWall curr_loc) 1)))
+
+    ;double movement
+    ;east east
+    (= dir 8) (and (can_travel player 0) (not (or (= (mod (+ (+ curr_loc 1) 1) width) 0) (= (nth rightWall (+ curr_loc 1)) 1))))
+
+    ;south south
+    (= dir 9) (and (can_travel player 1) (not (or (>= (+ (+ curr_loc width) width) (* width height)) (= (nth downWall (+ curr_loc width)) 1))))
+
+    ;west west
+    (= dir 10) (and (can_travel player 2) (not (or (= (mod (- curr_loc 1) width) 0) (= (nth rightWall (- (- curr_loc 1) 1)) 1))))
+
+    ;north north
+    (= dir 11) (and (can_travel player 3) (not (or (< (- curr_loc width) width) (= (nth downWall (- (- curr_loc width) width)) 1))))
 
     ;northeast
     (= dir 4) (or (and (can_travel player 3) (not (or (= (mod (+ (- curr_loc width) 1) width) 0) (= (nth rightWall (- curr_loc width)) 1))))
@@ -223,9 +239,10 @@
         w (can_travel player 2)
         n (can_travel player 3)]
     (println)
-      (println "=============================")
-      (println (-> the-maze loc :title))
-      (println "=============================")
+      (println "=================================")
+      (print (-> the-maze loc :title)) 
+      (println " (Enter i for instructions)")
+      (println "=================================")
        (cond (and (or east e) (or south s) (or west w) (or north n)) (println-typing "You are in an open area." 40)
         (and (or s south) (or w west) (or n north)) (println-typing "You are at an edge. There is a wall east of you." 40)
         (and (or e east) (or west w) (or n north)) (println-typing "You are at an edge. There is a wall south of you." 40)
@@ -237,10 +254,10 @@
         (and (or w west) (or n north)) (println-typing "You are at a corner. There are walls surrounding south and east." 40)
         (and (or s south) (or n north)) (println-typing "You are in an alley. Either head north or south" 40)
         (and (or w west) (or e east)) (println-typing "You are in an alley. Either head east or west." 40)
-        (and (or e east)) (println-typing "You cannot move forward. You are at a dead end." 40)
-        (and (or s south)) (println-typing "You cannot move forward. You are at a dead end." 40)
-        (and (or w west)) (println-typing "You cannot move forward. You are at a dead end." 40)
-        (and (or n north)) (println-typing "You cannot move forward. You are at a dead end." 40)
+        (and (or e east)) (println-typing "You cannot move forward. You are at a dead end. You have walls surrounding west, north and south." 40)
+        (and (or s south)) (println-typing "You cannot move forward. You are at a dead end. You have walls surrounding west, north and east. " 40)
+        (and (or w west)) (println-typing "You cannot move forward. You are at a dead end. You have walls surrounding east, north and south." 40)
+        (and (or n north)) (println-typing "You cannot move forward. You are at a dead end. You have walls surrounding west, east and south." 40)
         )) player
 )
 
@@ -306,7 +323,9 @@
         [:exit] (do (assoc-in player [:location] :exit ))
         [:end] (do (assoc-in player [:location] :exit  ))
         [:continue] (do (display (assoc-in (assoc-in (assoc-in (assoc-in player [:has-map] false) 
-                          [:has-key] false) [:location] loc) [:n] n))) _ player))
+                          [:has-key] false) [:location] loc) [:n] n))) 
+        _ (do (println "I don't know what you're saying. You are going to be respawned this room again.") (display (assoc-in (assoc-in (assoc-in (assoc-in player [:has-map] false) 
+                          [:has-key] false) [:location] loc) [:n] n)))))
     )
 
 ;this function checks if the player can travel in the maze
@@ -351,9 +370,14 @@
     nextmap (-> the-maze loc :nextmap)
     n (-> the-maze nextmap :n)]
 
+    
+
     (if (= nextmap :exit) (do (the_end) (assoc-in player [:location] nextmap))
 
-    (display (assoc-in (assoc-in (assoc-in (assoc-in player [:has-map] false) [:has-key] false) [:location] nextmap) [:n] n))))
+    (do 
+      (println)(println-typing (-> the-maze nextmap :desc) 10)
+      (display (assoc-in (assoc-in (assoc-in (assoc-in player [:has-map] false) [:has-key] false) [:location] nextmap) [:n] n)) 
+      )))
 )
 
 
@@ -388,9 +412,10 @@
  8  8, ,8         ,aaaaaaaaaaaaaaaaaaaaaaaaddddd88P
  8  `\"\"\"'       ,d8\"\"
  Yb,         ,ad8\"    
-  \"Y8888888888P\"\n") (println-typing "Do you want to take it?" 20) 
+  \"Y8888888888P\"\n") (println-typing "Do you want to take it? (y/n)" 20) 
       (match (to-keywords (read-line)) 
         [:yes] (do (println-typing "Key taken!" 40) (assoc-in player [:has-key] true))
+        [:y] (do (println-typing "Key taken!" 40) (assoc-in player [:has-key] true))
         _ player))
 
     (and (= maploc (player :n)) (not (player :has-map))) (do (println-typing "There is a map on the floor." 40)(println     
@@ -410,13 +435,17 @@
 ()==(                                            (@==()
      '--------------------------------------------'      \n")
 
-      (println-typing "Do you want to take it?" 20) 
+      (println-typing "Do you want to take it? (y/n)" 20) 
       (match (to-keywords (read-line)) 
         [:yes] (do (println-typing "You have obtained a map! to use the map type 'use map'" 40)(assoc-in player [:has-map] true))
+        [:y] (do (println-typing "You have obtained a map! to use the map type 'use map'" 40)(assoc-in player [:has-map] true))
         _ player))
 
-     (= exitloc (player :n)) (do (println "You have found the EXIT!!") (println "Do you want to open it?") (match (to-keywords (read-line)) 
+     (= exitloc (player :n)) (do (println "You have found the EXIT!!") (println "Do you want to open it? (y/n)") (match (to-keywords (read-line)) 
         [:yes] (do (if (player :has-key) 
+          (do (println "You have used your key to unlock the exit door.") (move-to player))
+          (do (println "It's locked!") player)))
+        [:y] (do (if (player :has-key) 
           (do (println "You have used your key to unlock the exit door.") (move-to player))
           (do (println "It's locked!") player)))
         _ player))
@@ -451,6 +480,7 @@
          [:n] (go 3 player)
          [:w] (go 2 player)
          [:i] (instructions player)
+         [:nothing] (do (println "Good you are doing nothing like always.") player)
          [:use :map] (do (if (player :has-map) (print-maze maze player) (println "You don't have a map yet.")) player)
          _ (do (println "I don't understand you.")
                player)
@@ -469,20 +499,20 @@
     (doseq [i (range monstn)]
       (cond 
         ;east
-        (and (can_travel player 0) (= (- (nth ((nth monster i) :path) ((nth monster i) :index)) (player :n)) 2)) (println "WARNING: There is a monster east of you")
+        (and (can_travel player 8) (= (- (nth ((nth monster i) :path) ((nth monster i) :index)) (player :n)) 2)) (println "WARNING: There is a monster east of you")
         (and (can_travel player 0) (= (- (nth ((nth monster i) :path) ((nth monster i) :index)) (player :n)) 1))(println "WARNING: Run!! There is a monster east of you")
 
         ;south
-        (and (can_travel player 1) (= (- (nth ((nth monster i) :path) ((nth monster i) :index)) (player :n)) (* 2 width)))(println "WARNING: There is a monster south of you")
+        (and (can_travel player 9) (= (- (nth ((nth monster i) :path) ((nth monster i) :index)) (player :n)) (* 2 width)))(println "WARNING: There is a monster south of you")
         (and (can_travel player 1) (= (- (nth ((nth monster i) :path) ((nth monster i) :index)) (player :n)) width)) (println "WARNING: Run!! There is a monster south of you")
 
         ;west
-        (and (can_travel player 2) (= (- (nth ((nth monster i) :path) ((nth monster i) :index)) (player :n)) -2)) (println "WARNING: There is a monster west of you")
+        (and (can_travel player 10) (= (- (nth ((nth monster i) :path) ((nth monster i) :index)) (player :n)) -2)) (println "WARNING: There is a monster west of you")
         (and (can_travel player 2) (= (- (nth ((nth monster i) :path) ((nth monster i) :index)) (player :n)) -1)) (println "WARNING: Run!! There is a monster west of you")
         
         ;north
-        (and (can_travel player 3) (= (- (nth ((nth monster i) :path) ((nth monster i) :index)) (player :n)) (* -2 width))) (println "WARNING: There is a monster north of you")
-        (and (can_travel player 3) (= (- (nth ((nth monster i) :path) ((nth monster i) :index)) (player :n)) (* -1 width))) (println "WARNING: Run!! There is a monster east of you")
+        (and (can_travel player 11) (= (- (nth ((nth monster i) :path) ((nth monster i) :index)) (player :n)) (* -2 width))) (println "WARNING: There is a monster north of you")
+        (and (can_travel player 3) (= (- (nth ((nth monster i) :path) ((nth monster i) :index)) (player :n)) (* -1 width))) (println "WARNING: Run!! There is a monster north of you")
 
         ;northeast
         (and (can_travel player 4) (= (- (nth ((nth monster i) :path) ((nth monster i) :index)) (player :n)) (+ (* -1 width) 1))) (println "WARNING: There is a monster northeast of you")
@@ -506,45 +536,47 @@
   (println)
 
 
-;   (println-typing " 
+  (println-typing " 
 
-;  ___      ___           __                                    __          
-; /   \\    /   \\  ____   |  |   ____   ____   _____   ____    _/  |_   ___  
-; \\    \\/\\/   /  / __ \\  |  |  / ___\\ /  _ \\ /     \\_/  __\\  |      \\ /  _ \\ 
-;  \\         /  \\  ___/  |  |_ \\ \\__  ( <_> )  Y Y  \\  ___/    |  |  (  <_> )
-;   \\__/\\   /    \\___ >  |____/ \\___ > ____/ |__| _| /\\___ >   |__|   \\____/ 
-;        \\_/        \\/             \\/              \\/   \\/                " 10)
+ ___      ___           __                                    __          
+/   \\    /   \\  ____   |  |   ____   ____   _____   ____    _/  |_   ___  
+\\    \\/\\/   /  / __ \\  |  |  / ___\\ /  _ \\ /     \\_/  __\\  |      \\ /  _ \\ 
+ \\         /  \\  ___/  |  |_ \\ \\__  ( <_> )  Y Y  \\  ___/    |  |  (  <_> )
+  \\__/\\   /    \\___ >  |____/ \\___ > ____/ |__| _| /\\___ >   |__|   \\____/ 
+       \\_/        \\/             \\/              \\/   \\/                " 5)
 
 
-;   (println-typing "                              
-;  _________________________________________________________________________
-; |                                                                         |
-; |         === = = ===   .-. .-. .=. .== .-. .  '. .' .--. .-= .==         |
-; |          |  |=| |=    |=| |=' | | |   |=| |    |   |--' `-. |=          |
-; |          =  = = ===   = = =   `=' `== = = `==  =   =    =-' `==         |
-; |_________________________________________________________________________|" 
-; 10)
-;   (println)
+  (println-typing "                              
+ _________________________________________________________________________
+|                                                                         |
+|         === = = ===   .-. .-. .=. .== .-. .  '. .' .--. .-= .==         |
+|          |  |=| |=    |=| |=' | | |   |=| |    |   |--' `-. |=          |
+|          =  = = ===   = = =   `=' `== = = `==  =   =    =-' `==         |
+|_________________________________________________________________________|" 
+5)
+  (println)
 
-; (println-typing "                        
-;  _____________________________________________________________________________
-; |                                                                             |
-; | The Apocalypse is a game of adventure, danger and monsters.                 |
-; | In this world you will explore some of the most amazing puzzles             |
-; | and mazes ever seen by mortal man.                                          |
-; |                                                                             |
-; | In Apocalypse the intrepid explorer finds himself in a lost labyrinth       |
-; | of another world, searching for a door that will take him back              |
-; | to his own world, his loved ones. But to find this door,                    |
-; | the adventurer has to go through different mazes filled with                |
-; | unknown creatures and traps!                                                |
-; |                                                                             |
-; | This game has been created by Shashank Bansal and Boom Dej-Udom.            |
-; | Have fun! and let us know if you have any feedback at sbansal6@illinois.edu.|
-; |                                                                             |
-; |_____________________________________________________________________________|" 10)
+(println-typing "                        
+ _____________________________________________________________________________
+|                                                                             |
+| The Apocalypse is a game of adventure, danger and monsters.                 |
+| In this world you will explore some of the most amazing puzzles             |
+| and mazes ever seen by mortal man.                                          |
+|                                                                             |
+| In Apocalypse the intrepid explorer finds himself in a lost labyrinth       |
+| of another world, searching for a door that will take him back              |
+| to his own world, his loved ones. But to find this door,                    |
+| the adventurer has to go through different mazes filled with                |
+| unknown creatures and traps!                                                |
+|                                                                             |
+| This game has been created by Shashank Bansal and Boom Dej-Udom.            |
+| Have fun! and let us know if you have any feedback at sbansal6@illinois.edu |
+| or dejudom2@illinois.edu.                                                   |
+|_____________________________________________________________________________|" 10)
   
   (def hero (assoc-in adventurer [:n] ((the-maze (adventurer :location)) :n)))
+  (println)
+  (println-typing ((the-maze (adventurer :location)) :desc) 10)
   (loop [local-maze the-maze
          local-player hero]
          ;(println "hello")
@@ -554,7 +586,7 @@
               pll (status mz pl)
               ]
               (if(not (= (pll :location) :exit))(let [
-                _ (print-maze mz pll)
+                ;_ (print-maze mz pll)
               _ (report-monster mz pll)
               _ (println-typing "What do you want to do now?" 20)
           command (read-line)]
